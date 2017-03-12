67b77bf80dc52e64e32da3a03a4ef119bd650f95
Julian Berman <Julian@GrayVines.com>
2017-03-12 09:30:44 -0400

    Matcherized.

--- process/tests/test_command.py
+++ process/tests/test_command.py
@@ -1,36 +1,36 @@
-from unittest import TestCase
-
 from filesystems import Path
+from mate import TestCase
 from process import Command
+from testtools.matchers import Equals
 
 
 class TestCommand(TestCase):
     def test_arguments(self):
         command = Command("foo", "bar")
-        self.assertEqual(
+        self.assertThat(
             command.arguments("--baz", "quux"),
-            Command("foo", "bar", "--baz", "quux"),
+            Equals(Command("foo", "bar", "--baz", "quux")),
         )
 
     def test_arguments_override(self):
         command = Command("foo", "bar", cwd=Path("tmp", "foo"))
-        self.assertEqual(
+        self.assertThat(
             command.arguments("baz", cwd=Path("tmp", "bar")),
-            Command("foo", "bar", "baz", cwd=Path("tmp", "bar")),
+            Equals(Command("foo", "bar", "baz", cwd=Path("tmp", "bar"))),
         )
 
     def test_prepended_arguments(self):
         command = Command("foo", "bar")
-        self.assertEqual(
+        self.assertThat(
             command.prepended_arguments("baz", "quux"),
-            Command("baz", "quux", "foo", "bar"),
+            Equals(Command("baz", "quux", "foo", "bar")),
         )
 
     def test_prepended_arguments_override(self):
         command = Command("foo", "bar", cwd=Path("tmp", "foo"))
-        self.assertEqual(
+        self.assertThat(
             command.prepended_arguments("baz", cwd=Path("tmp", "bar")),
-            Command("baz", "foo", "bar", cwd=Path("tmp", "bar")),
+            Equals(Command("baz", "foo", "bar", cwd=Path("tmp", "bar"))),
         )
 
     def test_random_extra_kwargs(self):
